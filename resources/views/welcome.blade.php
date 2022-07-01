78d6ec7f2d5f3c7d7ff4919a33e59365206bf56f
edmundchan8 <pokedev8@gmail.com>
2022-06-30 23:49:43 -0700

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edmunds Website</title>
-    <script defer src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <div id="root"></div>
+    <script defer src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
